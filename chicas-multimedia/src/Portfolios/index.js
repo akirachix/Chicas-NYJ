dccf657b3635d1d4434ab1da10e1a3f7f4f5221b
Ynonne-Martin <yvonnemartinkatumbi@gmail.com>
2024-07-13 23:10:00 +0300

    added portifolio page

---
+++ chicas-multimedia/src/Portfolios/index.js
@@ -0,0 +1,77 @@
+import './index.css'
+const Portfolio = ()=>{
+    return(
+    
+        <div className='team'>
+         <h1><b>MEET the TEAM</b></h1>
+  
+        
+        
+        <div id='Team_pictures'>
+            
+            
+            <div className='jemimahYvonne'>
+
+        <div id='jemimah'>
+            <fig>
+            <img src="/images/faith.png" alt=''/>
+            <figcaption><b>Faith W.</b></figcaption>
+            <figcaption>Software Developer</figcaption>
+            <figcaption><a href='http://faith-portfolio-six.vercel.app/'>View Portfolio</a></figcaption>
+            
+            </fig>
+        </div>
+
+         <div id='yvonne'>
+            <fig>
+            <img src='/images/yvonne1.png' alt=''/>
+            <figcaption><b>Yvonne M.</b></figcaption>
+            <figcaption>Software Developer</figcaption>
+            <figcaption><a href='https://yvonne-s-portfolio-git-master-yvonne-martins-projects.vercel.app/'>View Portfolio</a></figcaption>
+
+           
+            </fig>
+            </div>
+            </div>
+            
+            <div id='fanny'>
+            <fig>
+            <img src='/images/mary.png' alt=''/>
+            <figcaption><b>Mary Vivian M.</b></figcaption>
+            <figcaption>Software Developer</figcaption>
+            <figcaption><a href='https://vivianportfolio.vercel.app/'>View Portfolio</a></figcaption>
+
+            </fig>
+            </div>
+
+            <div className='rehema'>
+            <div id='rehema'>
+            <fig>
+            <img src='/images/rachael.png' alt=''/>
+            <figcaption><b>Rachael A.</b></figcaption>
+            <figcaption>Software Developer</figcaption>
+           
+            </fig>
+            </div>
+            
+
+            <div id='yiamat'>
+            <fig>
+            <img src='/images/njeri.png' alt=''/>
+            <figcaption><b>Maureen N.</b></figcaption>
+            <figcaption>Software Developer</figcaption>
+            <figcaption><a href='https://my-portfolio-gamma-nine-78.vercel.app/'>View Portfolio</a></figcaption>
+
+            </fig>
+            </div>
+            </div>
+           
+        </div>
+       
+        </div>
+    
+
+    );
+}
+export default Portfolio
+
